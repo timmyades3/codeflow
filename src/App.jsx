83ebf9d35c7ebc7617c1f0e214283e769757081c
timmyades3 <oluwatimileyinadesina623@gmail.com>
2026-04-04 23:05:02 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-950 text-white overflow-hidden">
+      <Navbar />
+      <Hero />
+      <Features />
+      <Pricing />
+      <Testimonial />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
