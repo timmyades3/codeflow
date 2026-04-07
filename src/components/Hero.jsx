23cf15f2f325e2a3fdfdae2eb726ee54e4d8580b
timmyades3 <oluwatimileyinadesina623@gmail.com>
2026-04-07 10:09:21 +0100

    finalizing hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import { ChevronDown, Sparkles } from "lucide-react";
+import { ArrowRight, ChevronDown, Play, Sparkles } from "lucide-react";
 import { useEffect, useState } from "react";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { codeExamples, floatingCards } from "../data/CodeExample";
@@ -32,29 +32,48 @@ export default function Hero() {
       <div className="absolute bottom-5 right-4 sm:right-10 w-64 sm:w-69 h-64 sm:h-96 bg-cyan-500/10 rounded-full blur-3xl animate-pulse"></div>
 
       <div className="max-w-7xl mx-auto text-center realtive w-full">
-       
-        <div className="max-w-7xl mx-auto flex flex-col lg:grid lg:grid-cols-2 gap-6 sm:gap-8 lg:gap-12 items-center relative">
+        <div className="max-w-7xl mx-auto flex flex-col lg:grid lg:grid-cols-2 text-center lg:text-left gap-6 sm:gap-8 lg:gap-12 items-center relative">
           <div className="">
-            <div className=" inline-flex items-center space-x-2 px-3 sm:px-4 py-2 bg-blue-500/10 border border-blue-500/20 rounded-full mb-4 sm:mb-6 animate-in slide-in-from-bottom duration-700">
+            <div className=" inline-flex items-center space-x-2 px-3 sm:px-4 py-2 bg-blue-500/10 border border-blue-500/20 rounded-full mb-4 sm:mb-6 animate-in slide-in-from-bottom duration-700 delay-100">
               <Sparkles className="w-4 h-4 text-blue-400" />
-              <span className="text-xs sm:text-sm text-blue-300 ">Introducing CodeFlow AI</span>
+              <span className="text-xs sm:text-sm text-blue-300 ">
+                Introducing CodeFlow AI
+              </span>
             </div>
-            <h1 className="text-5xl sm:text-3xl md:text-4xl lg:text-5xl font-semibold">
-              <span className="">
+            <h1 className="text-5xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-semibold mb-4 sm:mb-6 animate-in slide-in-from-bottom duration-700 delay-100">
+              <span className="bg-linear-to-r from-white via-blue-100 to-cyan-100 bg-clip-text text-transparent block mb-1 sm:2 ">
                 Code Faster
               </span>
-              <span className="">
+              <span className="bg-linear-to-b from-blue-400 via-cyan-400 to-blue-400 bg-clip-text text-transparent block mb-1 sm:2 ">
                 Build Better
               </span>
-              <span className="">
+              <span className="bg-linear-to-r from-white via-blue-100 to-cyan-100 bg-clip-text text-transparent block mb-1 sm:2 ">
                 With CodeFlow AI
               </span>
-              
             </h1>
+            <p className="text-md  sm:text-base lg:text-lg text-gray-400 max-w-2xl mx-auto lg:mx-0 mb-6 sm:mb-8 animate-in slide-in-from-bottom duration-700 delay-200 leading-relaxed">
+              Accelerate your development workflow with intelligent code
+              completion, automated testing, and smart debugging. Ship
+              production-ready code 10x faster.
+            </p>
+
+            <div className="flex flex-col sm:flex-row items-center justify-center lg:justify-start gap-3 sm:gap-4 mb-8 sm:mb-12 animate-in slide-in-from-bottom duration-700 delay-300 ">
+              <button className="group w-full sm:w-auto px-6 sm:p-8 py-3 sm:py-4 bg-linear-to-b from-blue-600 to-blue-400 rounded-lg font-semibold text-sm sm:text-base transition-all duration-300 hover:scale-102 flex items-center justify-center space-x-2 ">
+                <span className="">Start Coding Free</span>
+                <ArrowRight className="w-4 h-4 sm:h-5 sm:w-5 group-hover:translate-x-1 transition-transform duration-300 " />
+              </button>
+
+              <button className="group w-full sm:w-auto px-6 sm:p-8 py-3 sm:py-4 bg-white/5 backdrop-blur-sm border border-white/10 rounded-lg font-semibold text-sm sm:text-base transition-all duration-300 hover:bg-white/10 flex items-center justify-center space-x-2 ">
+                <div className="p-2 bg-white/10 rounded-full group-hover:bg-white/20 duration-300 transition-colors">
+                  <Play className="w-4 h-4 sm:h-5 sm:w-5 fill-white" />
+                </div>
+                <span className="">Watch Demo</span>
+              </button>
+            </div>
           </div>
           <div className="relative order-2 w-full">
             <div className="relative bg-white/5 backdrop-blur-xl rounded-xl sm:rounded-2xl p-3 sm:p-4 shadow-2xl border border-white/10">
-              <div className="bg-linear-to-br from-gray-900/20 to-gray-800/20  backdrop-blur-sm rounded-lg overflow-hidden h-70 sm:w-[360] lg:h-112.5 border border-white/5">
+              <div className="bg-linear-to-br from-gray-900/20 to-gray-800/20  backdrop-blur-sm rounded-lg overflow-hidden h-70 sm:h-87.5 lg:h-112.5 border border-white/5 text-left">
                 {/* IDE HEADER */}
                 <div className="flex items-center justify-between px-3 sm:px-4 py-2 sm:py-3 bg-white/5 backdrop-blur-sm border-b border-white/">
                   <div className="flex items-center space-x-1 sm:space-x-2">
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <nav className="fixed top-0 w-full transition-all duration-300 bg-slate-950/20 backdrop-blur-sm border-b-amber ">
+    <nav className="fixed top-0 w-full z-50 transition-all duration-300 bg-slate-950/20 backdrop-blur-sm border-b-amber ">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 ">
         <div className="flex justify-between items-center h-14 sm:h-16 md:h-20">
           <div className=" flex items-center space-x-1 group cursor-pointer ">
@@ -46,7 +46,7 @@ export default function Navbar() {
             </a>
           </div>
           <button
-            className="md:hidden p-2 text-gary-300 hover:text-white "
+            className="md:hidden p-2 text-gray-300 hover:text-white "
             onClick={() => setMobileMenuOpen((prev) => !prev)}
           >
             {mobileMenuOpen ? (
@@ -57,8 +57,8 @@ export default function Navbar() {
           </button>
         </div>
       </div>
-      {mobileMenuOpen && (
-        <div className="md:hidden bg-slate-900/95 backdrop:blur-lg border-t border-slate-800 animate-in slide-in-from-top duration-300 ">
+          {mobileMenuOpen && (
+        <div className="md:hidden bg-slate-900/95 backdrop-blur-lg border-t border-slate-800 animate-in slide-in-from-top duration-300 ">
           <div className="px-4 py-4 sm:py-6 space-y-3 sm:space-y-4 ">
             <a
               href="#features"
